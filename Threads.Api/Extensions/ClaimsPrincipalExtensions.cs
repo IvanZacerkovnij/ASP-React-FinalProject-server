28a54b609f59d36cf0da24e946a5c53bf6ed7bc1
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-09-12 10:24:32 +0300

    refactor

--- Threads.Api/Extensions/ClaimsPrincipalExtensions.cs
+++ Threads.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -2,14 +2,14 @@ using System.Security.Claims;
 
 namespace Threads.Api.Extensions;
 
-public static class ClaimPrincipalExtension
+public static class ClaimsPrincipalExtensions
 {
     public static Guid? GetCurrentUserId(this ClaimsPrincipal user)
     {
         var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
-        
-        return Guid.TryParse(userId, out var parsedUserId) 
-            ? parsedUserId 
+
+        return Guid.TryParse(userId, out var parsedUserId)
+            ? parsedUserId
             : null;
     }
 }
